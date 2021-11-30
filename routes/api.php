c239535184a4a3428faeaeebdfa9d91c61840153
edogalihr <edogalihr7@gmail.com>
2021-11-30 10:12:02 +0700

    Membuat REST API CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProgramController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,5 +29,6 @@ Route::get('/profile', function(Request $request) {
     return auth()->user();
 });
 // API route for logout user
+Route::resource('programs', ProgramController::class);
 Route::post('/logout', [AuthController::class, 'logout']);
 });
